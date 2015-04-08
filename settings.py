8b3dbc249ccab52eb4315080a67d602168d6be75
Jon Poler <jdp5087@gmail.com>
2015-04-08 08:55:56 -0400

    settings now has a SERVER_IP variable which comes from an env var.

--- settings.py
+++ settings.py
@@ -7,5 +7,6 @@ MAX_GPS_ERROR_TOLERANCE = 20 # in meters, arbitrary choice
 ALERT_DISTANCE = 200 # in meters, also arbitrary
 USERNAME = os.environ['AUTOMATIC_TEST_USERNAME']
 OS_USERNAME = 'jdp'
+SERVER_IP = os.environ['JPOLER_SERVER_IP']
 PORT = 5000
 
